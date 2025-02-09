7bc4753b53d6d1d91e1f9d7dd6d54140aba52577
Oleksandr Radchenko <oradchenko108@gmail.com>
2025-02-09 19:01:00 +0530

    create orders list view, template

--- src/orders/views.py
+++ src/orders/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.views.generic import ListView
 
-# Create your views here.
+from .models import Order
+
+
+class OrderListView(ListView):
+    model = Order
+    template_name = 'orders/order_list.html'
+    context_object_name = 'orders'
+
+    def get_queryset(self):
+        """Filter orders by table number or status (if specified in the request)"""
+        queryset = Order.objects.all()
+        table_number = self.request.GET.get('table_number')
+        status = self.request.GET.get('status')
+
+        if table_number:
+            queryset = queryset.filter(table_number=table_number)
+        if status:
+            queryset = queryset.filter(status=status)
+
+        return queryset
