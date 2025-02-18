3956dfb954a536d37b1c8f92f35d1e777688a918
Oleksandr Radchenko <oradchenko108@gmail.com>
2025-02-18 14:30:45 +0530

    add description

--- src/orders/templatetags/currency.py
+++ src/orders/templatetags/currency.py
@@ -5,6 +5,7 @@ register = template.Library()
 
 @register.filter
 def rubles(value):
+    """Converts price from kopecks to rubles."""
     try:
         return float(value) / 100
     except (ValueError, TypeError):
